6746b74195fd17638e274bbf6fe3e0c8c6d7de4e
ezoza <ezozakhudayberdieva@mail.ru>
2025-01-17 21:50:02 +0300

    fix: change component register add alert information

--- src/components/smartComponents/registration/Registration.jsx
+++ src/components/smartComponents/registration/Registration.jsx
@@ -3,7 +3,7 @@ import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, T
 import { MyButton } from "../../stupidComponents/button/MyButton"
 import { butonTheme } from "../../../styles/Theme"
 
-export const Register = ({setArray, arrayUserLocalStorage}) => {
+export const Register = () => {
     const [open, setOpen] = React.useState(false);
     const [nameValue, setNameValue] = React.useState('');
     const [passValue, setPassValue] = React.useState('');
@@ -15,16 +15,18 @@ export const Register = ({setArray, arrayUserLocalStorage}) => {
          const handleClose = () => {
             setOpen(false)
          }
-         const handleRegistration = () => {
+         const handleRegistration = (props) => {
                 const checkUserName = () => {
                     if(localStorage.getItem(nameValue)) {
-                        alert('пользователь уже существует');
+                        alert('Пользователь с таким именим уже существует ')
                     } else {
                         localStorage.setItem(`${nameValue}`, JSON.stringify([{name: nameValue, password: passValue}]));
+                        alert('Ура вы зарегистрировались нажмите на кнопку вход для входа в приложение');
                         handleClose()
                     }
                 }
-                passValue === passCheckValue ? checkUserName() : alert('неверный пароли 1 и 2');
+                passValue.length < 4 ? alert('Слишком короткий пороль введите чуть по длиннее') :
+                passValue === passCheckValue ? checkUserName() : alert('Пороли не совпадают попробуйте снова');
          } 
             
     return (
@@ -33,7 +35,7 @@ export const Register = ({setArray, arrayUserLocalStorage}) => {
             <Dialog open={open} onClose={handleClose} aria-labelledby="registrationDialog"
                PaperProps={{component: 'form', onSubmit: (event)=>{ 
                 event.preventDefault();
-                handleRegistration()}}}>
+                handleRegistration ()}}}>
                 <DialogTitle id='registrationDialog' />
                 <DialogContent>
                 <DialogContentText>Log in to see video</DialogContentText>
