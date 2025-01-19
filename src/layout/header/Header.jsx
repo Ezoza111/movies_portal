db050aa4eafeb37869d59233b1e07fff8b1b2efd
ParaPaca <svetaalex8@gmail.com>
2025-01-19 08:00:32 +0300

    formatted code and added SearchForm

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,41 +1,51 @@
 import React from "react";
-import styled from 'styled-components'
-import { Logo } from '../../components/stupidComponents/logo/Logo'
-import { MainContainer } from '../../components/stupidComponents/container/MainContainer.styled'
-import { FavoritesLink } from '../../components/stupidComponents/favorites/FavoritesLink'
-import { Login } from '../../components/smartComponents/login/Login'
-import { Entrance } from '../../components/smartComponents/entrance/Entrance'
-
-
-
-
-
+import styled from "styled-components";
+import { Logo } from "../../components/stupidComponents/logo/Logo";
+import { MainContainer } from "../../components/stupidComponents/container/MainContainer.styled";
+import { FavoritesLink } from "../../components/stupidComponents/favorites/FavoritesLink";
+import { Login } from "../../components/smartComponents/login/Login";
+import { Entrance } from "../../components/smartComponents/entrance/Entrance";
+import SearchForm from "../../components/smartComponents/searchForm/SearchForm";
 
 export const Header = () => {
-   const [loginStatus, setLoginStatus] = React.useState(true);
-   const loginStatusFalse = () => {
-    setLoginStatus(false)
+  const [loginStatus, setLoginStatus] = React.useState(true);
+  const loginStatusFalse = () => {
+    setLoginStatus(false);
   };
-  const loginStatusTrue= () => {
-    setLoginStatus(true)
+  const loginStatusTrue = () => {
+    setLoginStatus(true);
   };
 
   return (
     <StyledHeader className='header'>
-      <MainContainer direction={'row'} justify={'space-between'} align={'center'}>
-        <Logo iconId={'logo'}/>
-        <FavoritesLink/>
-        <Entrance  loginStatusTrue = {loginStatusTrue} loginStatusFalse= {loginStatusFalse}/>
-        {loginStatus ? <Login loginStatusTrue = {loginStatusTrue} loginStatusFalse= {loginStatusFalse}/>: ''}
+      <MainContainer
+        direction={"row"}
+        justify={"space-between"}
+        align={"center"}>
+        <Logo iconId={"logo"} />
+        <SearchForm />
+        <FavoritesLink />
+        <Entrance
+          loginStatusTrue={loginStatusTrue}
+          loginStatusFalse={loginStatusFalse}
+        />
+        {loginStatus ? (
+          <Login
+            loginStatusTrue={loginStatusTrue}
+            loginStatusFalse={loginStatusFalse}
+          />
+        ) : (
+          ""
+        )}
       </MainContainer>
     </StyledHeader>
-  )
-}
+  );
+};
 
 const StyledHeader = styled.header`
-    width: 100%;
-    height: 80px;
-    border: 1px solid red;
-    display: flex;
-    align-items: center;
-`
+  width: 100%;
+  height: 80px;
+  border: 1px solid red;
+  display: flex;
+  align-items: center;
+`;
