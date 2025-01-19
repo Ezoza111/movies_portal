1156f2ebc57c3d008e0d84b3e4d1a6ef60056c7a
ParaPaca <svetaalex8@gmail.com>
2025-01-19 09:37:24 +0300

    refactor: formatted code and renamed buttonTheme

--- src/components/smartComponents/entrance/Entrance.jsx
+++ src/components/smartComponents/entrance/Entrance.jsx
@@ -1,84 +1,107 @@
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
 import React from "react";
-import { butonTheme } from "../../../styles/Theme";
+import { buttonTheme } from "../../../styles/Theme";
 import { ThemeProvider } from "@emotion/react";
 import { MyButton } from "../../stupidComponents/button/MyButton";
 
 // вход
-export const Entrance = ({loginStatusTrue, loginStatusFalse}) => {
-   const [open, setOpen] = React.useState(false)
-   const [nameValue, setNameValue] = React.useState('');
-   const [passValue, setPassValue] = React.useState('');
-   const [userOutAccaunt, setUserOutAccaunt] = React.useState(true)
-   const signAccaunt = () => {
-    setUserOutAccaunt(false)
-}
-const outAccaunt = () => {
-  setUserOutAccaunt(true)
-}
-    const handleClickOpen = () => {
-       setOpen(true)
-    }
-    const handleClose = () => {
-      setOpen(false)
-   }
-   const handleSignIn = () => {
-      const enterAkkaunt = () => {
-        const user = JSON.parse(localStorage.getItem(nameValue));
-        if (user[0].password === passValue) {
-          loginStatusFalse()
-          signAccaunt()
-        }
-          else { alert('Ой не правильный пороль')};
+export const Entrance = ({ loginStatusTrue, loginStatusFalse }) => {
+  const [open, setOpen] = React.useState(false);
+  const [nameValue, setNameValue] = React.useState("");
+  const [passValue, setPassValue] = React.useState("");
+  const [userOutAccaunt, setUserOutAccaunt] = React.useState(true);
+  const signAccaunt = () => {
+    setUserOutAccaunt(false);
+  };
+  const outAccaunt = () => {
+    setUserOutAccaunt(true);
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleSignIn = () => {
+    const enterAkkaunt = () => {
+      const user = JSON.parse(localStorage.getItem(nameValue));
+      if (user[0].password === passValue) {
+        loginStatusFalse();
+        signAccaunt();
+      } else {
+        alert("Ой не правильный пороль");
       }
-      localStorage.getItem(nameValue) ? enterAkkaunt() : alert('Пользователь с таким именим не существует')
-    }
-    const exitUserAccaunt =() => {
-      loginStatusTrue();
-      outAccaunt();
-    }
-  return (
-    userOutAccaunt ?  
-      <ThemeProvider theme={butonTheme}>
-        <MyButton name='Sign In' functionClick={handleClickOpen} />
-        <Dialog Dialog open={open} onClose={handleClose} aria-labelledby="enrtanceDialog"
-      PaperProps={{component: 'form', onSubmit: ()=>handleSignIn()}}>
-    <DialogTitle id="enrtanceDialog"/>
-    <DialogContent>
-      <DialogContentText>Entrance to see video</DialogContentText>
-      <TextField autoFocus
-        required
-        margin="dence"
-        id="name"
-        value={nameValue}
-        onChange={(event) => {setNameValue(event.target.value)}}
-        label='User Name'
-        type="text"
-        fullWidth
-        />
-      <TextField autoFocus
-        required
-        margin="dence"
-        id="pass"
-        value={passValue}
-        onChange={(event) => {event.preventDefault(); setPassValue(event.target.value)}}
-        label='Password'
-        type="Password"
-        fullWidth
-      />
-    </DialogContent>
-    <DialogActions>
-    <MyButton name='Sign in' typeBtn='submit' />
-    <MyButton functionClick={handleClose} name='Cancel' />
-    </DialogActions>
+    };
+    localStorage.getItem(nameValue)
+      ? enterAkkaunt()
+      : alert("Пользователь с таким именим не существует");
+  };
+  const exitUserAccaunt = () => {
+    loginStatusTrue();
+    outAccaunt();
+  };
+  return userOutAccaunt ? (
+    <ThemeProvider theme={buttonTheme}>
+      <MyButton name='Sign In' functionClick={handleClickOpen} />
+      <Dialog
+        Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='enrtanceDialog'
+        PaperProps={{ component: "form", onSubmit: () => handleSignIn() }}>
+        <DialogTitle id='enrtanceDialog' />
+        <DialogContent>
+          <DialogContentText>Entrance to see video</DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin='dence'
+            id='name'
+            value={nameValue}
+            onChange={(event) => {
+              setNameValue(event.target.value);
+            }}
+            label='User Name'
+            type='text'
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            required
+            margin='dence'
+            id='pass'
+            value={passValue}
+            onChange={(event) => {
+              event.preventDefault();
+              setPassValue(event.target.value);
+            }}
+            label='Password'
+            type='Password'
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <MyButton name='Sign in' typeBtn='submit' />
+          <MyButton functionClick={handleClose} name='Cancel' />
+        </DialogActions>
       </Dialog>
-    
-    </ThemeProvider> : 
-    <ThemeProvider theme={butonTheme}>
-    <> <Box component='h3'>{nameValue.toLocaleUpperCase()}</Box>
-    <MyButton name='Exit' functionClick={exitUserAccaunt} /> 
- </>
-     </ThemeProvider>
-  )
-}
-
+    </ThemeProvider>
+  ) : (
+    <ThemeProvider theme={buttonTheme}>
+      <>
+        {" "}
+        <Box component='h3'>{nameValue.toLocaleUpperCase()}</Box>
+        <MyButton name='Exit' functionClick={exitUserAccaunt} />
+      </>
+    </ThemeProvider>
+  );
+};
--- src/components/smartComponents/registration/Registration.jsx
+++ src/components/smartComponents/registration/Registration.jsx
@@ -1,80 +1,112 @@
 import React from "react";
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, ThemeProvider, TextField } from "@mui/material"
-import { MyButton } from "../../stupidComponents/button/MyButton"
-import { butonTheme } from "../../../styles/Theme"
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  ThemeProvider,
+  TextField,
+} from "@mui/material";
+import { MyButton } from "../../stupidComponents/button/MyButton";
+import { buttonTheme } from "../../../styles/Theme";
 
 export const Register = () => {
-    const [open, setOpen] = React.useState(false);
-    const [nameValue, setNameValue] = React.useState('');
-    const [passValue, setPassValue] = React.useState('');
-    const [passCheckValue, setPassCheckValue] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+  const [nameValue, setNameValue] = React.useState("");
+  const [passValue, setPassValue] = React.useState("");
+  const [passCheckValue, setPassCheckValue] = React.useState("");
 
-        const handleClickOpen = () => {
-            setOpen(true)
-         }
-         const handleClose = () => {
-            setOpen(false)
-         }
-         const handleRegistration = (props) => {
-                const checkUserName = () => {
-                    if(localStorage.getItem(nameValue)) {
-                        alert('Пользователь с таким именим уже существует ')
-                    } else {
-                        localStorage.setItem(`${nameValue}`, JSON.stringify([{name: nameValue, password: passValue}]));
-                        alert('Ура вы зарегистрировались нажмите на кнопку вход для входа в приложение');
-                        handleClose()
-                    }
-                }
-                passValue.length < 4 ? alert('Слишком короткий пороль введите чуть по длиннее') :
-                passValue === passCheckValue ? checkUserName() : alert('Пороли не совпадают попробуйте снова');
-         } 
-            
-    return (
-        <ThemeProvider theme={butonTheme}>
-            <MyButton name='Log in' functionClick={handleClickOpen}></MyButton>
-            <Dialog open={open} onClose={handleClose} aria-labelledby="registrationDialog"
-               PaperProps={{component: 'form', onSubmit: (event)=>{ 
-                event.preventDefault();
-                handleRegistration ()}}}>
-                <DialogTitle id='registrationDialog' />
-                <DialogContent>
-                <DialogContentText>Log in to see video</DialogContentText>
-                <TextField autoFocus
-                        required
-                        margin="normal"
-                        id="name"
-                        value={nameValue}
-                        onChange={(event) => {setNameValue(event.target.value)}}
-                        label='User Name'
-                        type="text"
-                        fullWidth
-                />
-                 <TextField autoFocus
-                        required
-                        margin="normal"
-                        id="pass"
-                        value={passValue}
-                        onChange={(event) => {setPassValue(event.target.value)}}
-                        label='Password'
-                        type="Password"
-                        fullWidth
-                />
-                 <TextField autoFocus
-                        required
-                        margin="dence"
-                        id="passCheck"
-                        value={passCheckValue}
-                        onChange={(event) => {setPassCheckValue(event.target.value)}}
-                        label='Password'
-                        type="Password"
-                        fullWidth
-                />
-                </DialogContent>
-                <DialogActions>
-                <MyButton name='Log in' typeBtn='submit' />
-                <MyButton functionClick={handleClose} name='Cancel'/>
-                </DialogActions>
-            </Dialog>
-        </ThemeProvider>
-    )
-}
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleRegistration = (props) => {
+    const checkUserName = () => {
+      if (localStorage.getItem(nameValue)) {
+        alert("Пользователь с таким именим уже существует ");
+      } else {
+        localStorage.setItem(
+          `${nameValue}`,
+          JSON.stringify([{ name: nameValue, password: passValue }])
+        );
+        alert(
+          "Ура вы зарегистрировались нажмите на кнопку вход для входа в приложение"
+        );
+        handleClose();
+      }
+    };
+    passValue.length < 4
+      ? alert("Слишком короткий пороль введите чуть по длиннее")
+      : passValue === passCheckValue
+      ? checkUserName()
+      : alert("Пороли не совпадают попробуйте снова");
+  };
+
+  return (
+    <ThemeProvider theme={buttonTheme}>
+      <MyButton name='Log in' functionClick={handleClickOpen}></MyButton>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='registrationDialog'
+        PaperProps={{
+          component: "form",
+          onSubmit: (event) => {
+            event.preventDefault();
+            handleRegistration();
+          },
+        }}>
+        <DialogTitle id='registrationDialog' />
+        <DialogContent>
+          <DialogContentText>Log in to see video</DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin='normal'
+            id='name'
+            value={nameValue}
+            onChange={(event) => {
+              setNameValue(event.target.value);
+            }}
+            label='User Name'
+            type='text'
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            required
+            margin='normal'
+            id='pass'
+            value={passValue}
+            onChange={(event) => {
+              setPassValue(event.target.value);
+            }}
+            label='Password'
+            type='Password'
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            required
+            margin='dence'
+            id='passCheck'
+            value={passCheckValue}
+            onChange={(event) => {
+              setPassCheckValue(event.target.value);
+            }}
+            label='Password'
+            type='Password'
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <MyButton name='Log in' typeBtn='submit' />
+          <MyButton functionClick={handleClose} name='Cancel' />
+        </DialogActions>
+      </Dialog>
+    </ThemeProvider>
+  );
+};
