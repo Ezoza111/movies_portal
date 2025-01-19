cd2e9d72a932c3f52f31589686e641a94bd209b5
ParaPaca <svetaalex8@gmail.com>
2025-01-19 09:41:45 +0300

    style: added styles to FilmCard

--- src/components/stupidComponents/filmCard/FilmCard.jsx
+++ src/components/stupidComponents/filmCard/FilmCard.jsx
@@ -1,13 +1,16 @@
 import styled from "styled-components";
+import { theme } from "../../../styles/Theme";
 
 const FilmCard = ({ title, year, rank, image }) => {
   return (
     <StyledFilmCard className='film-card'>
-      <img src={image} alt={title} />
-      <h2>
-        {rank}. {title}
-      </h2>
-      <p>Year: {year}</p>
+      <img loading='lazy' src={image} alt={title} />
+      <div className='text-container'>
+        <h2>
+          {rank}. {title}
+        </h2>
+        <p>{year}</p>
+      </div>
     </StyledFilmCard>
   );
 };
@@ -15,24 +18,29 @@ const FilmCard = ({ title, year, rank, image }) => {
 export default FilmCard;
 
 const StyledFilmCard = styled.div`
+  width: 200px;
   display: flex;
   flex-direction: column;
   gap: 10px;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+  background-color: ${theme.secondary};
+  border-radius: 4px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   transition: box-shadow 0.3s;
   cursor: pointer;
 
   &:hover {
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    box-shadow: 0 0 20px rgba(255, 255, 255, 0.3);
   }
 
   img {
-    width: 50px;
-    height: auto;
-    border-radius: 5px;
+    border-radius: 4px 4px 0 0;
+    width: 100%;
+    height: 290px;
+    object-fit: cover;
+  }
+  .text-container {
+    padding: 15px;
+    gap: 5px;
   }
 
   h2 {
