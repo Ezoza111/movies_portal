d9c846581cf2382af7595f067ba374e72ebdcc86
ParaPaca <svetaalex8@gmail.com>
2025-01-19 08:00:53 +0300

    renamed Main.js to Main.jsx

---
+++ src/layout/main/Main.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import { MainContainer } from "../../components/stupidComponents/container/MainContainer.styled";
+import FilmList from "../../components/smartComponents/filmList/FilmList";
+
+export const Main = () => {
+  return (
+    <StyledMain className='main'>
+      <MainContainer direction={"column"} gap={"40px"} justify={"center"}>
+        <FilmList />
+      </MainContainer>
+    </StyledMain>
+  );
+};
+
+const StyledMain = styled.main`
+  width: 100%;
+  border: 1px solid red;
+  padding-top: 30px;
+  padding-bottom: 30px;
+`;
