fbf5b63b092105a755d21d08c9c84a36e87bfa88
ParaPaca <svetaalex8@gmail.com>
2025-01-21 16:18:05 +0300

    refactor: added and used useCallback in custom hook

--- src/components/smartComponents/customHooks/useLocalStorage.jsx
+++ src/components/smartComponents/customHooks/useLocalStorage.jsx
@@ -1,18 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react"; 
+//Использование useCallback для мемоизации функции getValue для предотвращения её создания заново при каждом рендере, если ключ не изменился.
 
-  export const useLocalStorage = (initialValue, key) => {
-    const getValue = () => {
-      const storage = localStorage.getItem(key);
-      if (storage) {
-        return JSON.parse(storage); // []
-      }
-      return initialValue;
-    }
-    const [value, setValue] = useState(getValue);
+export const useLocalStorage = (initialValue, key) => {
+  const getValue = useCallback(() => {
+    const storage = localStorage.getItem(key);
+    return storage ? JSON.parse(storage) : initialValue;
+  }, [key]); // Зависящая от key функция для получения значения
 
-    useEffect(() => { localStorage.setItem(key, JSON.stringify(value))}, [value])
-   
-    return [value, setValue];
- }
+  const [value, setValue] = useState(getValue);
 
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]); // Добавляем key как зависимость
 
+  return [value, setValue];
+};
