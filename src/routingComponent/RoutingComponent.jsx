28b595c0e2a6355d45fec992706444f4c18de4f2
ezoza <ezozakhudayberdieva@mail.ru>
2025-01-25 00:27:17 +0300

    refactor: RouingComponent change text

--- src/routingComponent/RoutingComponent.jsx
+++ src/routingComponent/RoutingComponent.jsx
@@ -1,11 +1,10 @@
 import {Outlet} from 'react-router-dom'
 import { Header } from '../layout/header/Header'
-import { useTheme } from 'styled-components'
 import { ThemeContext } from '../components/smartComponents/context/ThemeContext'
 import { useContext } from 'react'
 
 export const RoutingComponent = ({ userName, changeUserStatus}) => {
- const {isDark, toggleTheme} = useContext(ThemeContext);
+ const {isDark} = useContext(ThemeContext);
   return (
     <>
      <Header  userName={userName} changeUserStatus={changeUserStatus}/>
