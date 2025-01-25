25d14fc997ab6f8f6d29490dcb92d2c560565907
ezoza <ezozakhudayberdieva@mail.ru>
2025-01-25 12:15:04 +0300

    refactor: add content for component Logo

--- src/layout/header/headerIcons/Logo.jsx
+++ src/layout/header/headerIcons/Logo.jsx
@@ -4,11 +4,13 @@ import { IconButton } from "@mui/material";
 import ApiIcon from '@mui/icons-material/Api';
 
 import { theme } from "../../../styles/Theme";
+import { ThemeContext, useTheme } from "../../../components/smartComponents/context/ThemeContext";
 
 
-export const Logo = ({classStyle}) => {
+export const Logo = () => {
+  const {isDark} = useTheme(ThemeContext)
   const navigate = useNavigate();
-  const styless = classStyle ? {color: theme.colors.accent} : {color: theme.colors.accentLight}
+  const styless = isDark ? {color: theme.colors.accent} : {color: theme.colors.accentLight}
 
   return (
     <IconButton style={styless} onClick={() => navigate('/')}>
