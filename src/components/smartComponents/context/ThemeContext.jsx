92069c36d737db891e9875df79fad2ddd179952e
ParaPaca <svetaalex8@gmail.com>
2025-01-26 11:37:33 +0300

    refactor: formatted code

--- src/components/smartComponents/context/ThemeContext.jsx
+++ src/components/smartComponents/context/ThemeContext.jsx
@@ -1,30 +1,29 @@
 import React, { createContext, useContext } from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 export const ThemeContext = createContext(undefined);
 
 export const useTheme = () => {
-    const context = useContext(ThemeContext);
-    if(!context) {
-        throw new Error('Context: ThemeContext Error')
-    } 
-    return context
-}
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("Context: ThemeContext Error");
+  }
+  return context;
+};
 
+export const ThemeContextProvider = ({ children }) => {
+  const [isDark, setIsDark] = React.useState(false);
 
-export const ThemeContextProvider = ({children}) => {
-    const [isDark, setIsDark] = React.useState(false);
-
-    const toggleTheme = () => {
-        setIsDark((prev) => !prev)
-    }
-    return (
-        <ThemeContext.Provider value={{isDark, toggleTheme}}>
-            {children}
-        </ThemeContext.Provider>
-    )
-}
-ThemeContext.propstype = {
+  const toggleTheme = () => {
+    setIsDark((prev) => !prev);
+  };
+  return (
+    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+ThemeContext.propsType = {
   isDark: PropTypes.bool,
-  toggleTheme: PropTypes.func
-}
+  toggleTheme: PropTypes.func,
+};
