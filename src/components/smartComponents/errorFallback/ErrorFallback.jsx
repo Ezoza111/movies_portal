2adb9fadf9c4a5ab59db2ba071adb34247dbdbd6
ezoza <ezozakhudayberdieva@mail.ru>
2025-01-27 11:12:45 +0300

    refactor: add style for component ErrorFallback

--- src/components/smartComponents/errorFallback/ErrorFallback.jsx
+++ src/components/smartComponents/errorFallback/ErrorFallback.jsx
@@ -1,15 +1,69 @@
-import { ErrorBoundary } from "react-error-boundary";
+import { dark } from "@mui/material/styles/createPalette";
+import { useContext } from "react";
+import styled from "styled-components";
+import { theme } from "../../../styles/Theme";
+import { ThemeContext } from "../context/ThemeContext";
 
 function ErrorFallback({ error, resetErrorBoundary }) {
-  // Call resetErrorBoundary() to reset the error boundary and retry the render.
+  const { isDark } = useContext(ThemeContext);
 
   return (
-    <div role="alert">
+    <StyledErrorBoundary
+      role="alert"
+      className={`errorBoundary ${isDark ? "dark" : "light"}`}
+    >
       <p>Something went wrong: </p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
+      <p style={{ color: "red" }}>{error.message}</p>
       <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
+    </StyledErrorBoundary>
   );
 }
 
-export {ErrorFallback};
+export { ErrorFallback };
+
+const StyledErrorBoundary = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+
+  &.dark {
+    background-color: ${theme.colors.primary};
+    color: ${theme.colors.font};
+
+      button {
+        background-color: transparent;
+        color: ${theme.colors.accent};
+        border: 1px solid ${theme.colors.accent};
+        width: 50%;
+        padding: 1em;
+        font-size: 2em;
+      }
+
+      p{
+        font-size: 2em;
+        color: ${theme.colors.accent};
+      }
+    }
+  &.light {
+    background-color: ${theme.colors.primaryLight};
+    color: ${theme.colors.accentLight};
+
+    button {
+      background-color: transparent;
+      border: 1px solid ${theme.colors.accentLight};
+      color: ${theme.colors.accentLight};
+      width: 50%;
+      padding: 1em;
+      font-size: 2em;
+    }
+    p {
+      font-size: 2em;
+      color: ${theme.colors.accentLight};
+    }
+  }
+`;
