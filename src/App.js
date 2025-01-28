3473dd533b1ac8378b50c0faee9b0b19eae0c8c2
ParaPaca <svetaalex8@gmail.com>
2025-01-28 19:32:05 +0300

    feat: added page 404

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import RoutingComponent from "./routingComponent/RoutingComponent";
 const Main = lazy(() => import ("./layout/main/Main"));
 const FavoritesPage = lazy(() => import("./layout/favoritesPage/FavoritesPage"));
 const SignUpPage = lazy(() => import("./layout/signUpPage/SignUpPage"));
+const Page404 = lazy(() => import("./layout/404/Page404"));
 
 function App() {
     return (
@@ -18,6 +19,7 @@ function App() {
           <Route index element={<Main />} />
           <Route path='/favorites' element={<FavoritesPage />} />
           <Route path='/login' element={<SignUpPage />} />
+          <Route path='*' element={<Page404 />} />
         </Route>
       </Routes>
     </ErrorBoundary>
