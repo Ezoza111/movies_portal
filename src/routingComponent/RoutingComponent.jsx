fa180a6ff01091444ba19ae274b5f22b22c16d3c
ParaPaca <svetaalex8@gmail.com>
2025-01-31 11:35:12 +0300

    feat: added loader to suspense

--- src/routingComponent/RoutingComponent.jsx
+++ src/routingComponent/RoutingComponent.jsx
@@ -5,6 +5,7 @@ import { ThemeContext } from "../components/smartComponents/context/ThemeContext
 import { useContext } from "react";
 import { theme } from "../styles/Theme";
 import { Suspense } from "react";
+import CustomLoader from "../components/stupidComponents/customLoader/CustomLoader";
 
 const RoutingComponent = () => {
   const { isDark } = useContext(ThemeContext);
@@ -15,7 +16,12 @@ const RoutingComponent = () => {
       <div className='page-content'>
         <Header />
         <main>
-          <Suspense fallback={<p>Loading ...</p>}>
+          <Suspense
+            fallback={
+              <StyledLoaderOverlay>
+                <CustomLoader />
+              </StyledLoaderOverlay>
+            }>
             <Outlet />
           </Suspense>
         </main>
@@ -36,6 +42,19 @@ const StyledPageContainer = styled.div`
     max-width: 1280px;
     margin: 0 auto;
   }
+`;
+
+const StyledLoaderOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.7);
+  z-index: 10;
 `;
 
 export default RoutingComponent;
