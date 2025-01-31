1e952794497125ea550b6c8bcd707f78010d5105
ParaPaca <svetaalex8@gmail.com>
2025-01-31 11:30:54 +0300

    refactor: deleted userName prop in Main style: fixed some styles

--- src/layout/main/Main.jsx
+++ src/layout/main/Main.jsx
@@ -2,23 +2,22 @@ import styled from "styled-components";
 import { MainContainer } from "../../components/stupidComponents/container/MainContainer.styled";
 import FilmList from "../../components/smartComponents/filmList/FilmList";
 
-const Main = ({ userName }) => {
+const Main = () => {
   return (
     <StyledMain className='film-container'>
       <MainContainer direction={"column"}>
-        <FilmList userName={userName} />
+        <FilmList />
       </MainContainer>
     </StyledMain>
   );
 };
 
 const StyledMain = styled.div`
-  // max-width: 1280px;
   display: flex;
   width: 100%;
   padding-top: 30px;
   padding-bottom: 30px;
-  // height: 80vh;
+  height: 100%;
 `;
 
 export default Main;
